642335ce81d093e71964d924e0b95b7fd4f4f54a
mehmetkocagoz <mehmetkocagz@outlook.com>
2023-11-23 16:57:30 +0300

    new API endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,jsonify,Response
-from db import getHouses
+from db import getHouses, getHousesWithCity
 import json
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def home():
 
 
 @app.route('/houses')
-def houses():
+def all_houses():
     
     houses_data = getHouses()
 
@@ -25,6 +25,34 @@ def houses():
     # Returning the response as JSON
     return Response(json_response, content_type='application/json; charset=utf-8')
 
+@app.route('/houseswithquery')
+def houses_with_query():
+    city = request.args.get('city')
+    date_from = request.args.get('datefrom')
+    date_to = request.args.get('dateto')
+    number_of_peoples = request.args.get('numberofpeople')
+
+    # Set default values for pagination
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('per_page', 5))
+
+    house_list_for_given_city = getHousesWithCity(city)
+
+    # Build pagination information
+    pagination_info = {
+        'page': page,
+        'per_page': per_page,
+        'total_pages': 1,  # Update this based on the actual total pages
+        'total_items': len(house_list_for_given_city),  # Update this based on the actual total items
+    }
+
+     # Construct the final response
+    response_data = {
+        'houses': house_list_for_given_city,
+        'pagination': pagination_info,
+    }
+
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run()
--- db.py
+++ db.py
@@ -19,6 +19,7 @@ def conn():
 
     except pyodbc.Error as ex:
         sqlstate = ex.args[1]
+        print(sqlstate)
         return f"Error connecting to the database. SQLState: {sqlstate}"
 
 def getHouses():
@@ -58,4 +59,40 @@ def getHouses():
         # Close the connection
         connection.close()
 
-houses = getHouses()
+
+def getHousesWithCity(city):
+    try:
+        # Establish a connection
+        connection = conn()
+
+        # Check if the connection is successful
+        if connection:
+            # Create a cursor from the connection
+            with connection.cursor() as cursor:
+                # Execute a SELECT query with a WHERE clause for the specified city
+                cursor.execute("SELECT * FROM houses WHERE CITY = ?", (city,))
+
+                # Fetch all the rows
+                rows = cursor.fetchall()
+                houses_data_list = []
+                for row in rows:
+                    house_entry = {
+                        'ID': row[0],
+                        'CITY': row[1],
+                        'DESCRIPTION': row[3],
+                        'AMENITIES': row[4],  # Corrected typo: 'AMENETIES' to 'AMENITIES'
+                        'MAX_PEOPLE': row[2],
+                        'IS_BOOKED': row[5]  # Adjusted to 'IS_BOOKED'
+                    }
+                    houses_data_list.append(house_entry)
+
+            return houses_data_list
+
+    except pyodbc.Error as ex:
+        sqlstate = ex.args[1]
+        print(f"Error executing the query. SQLState: {sqlstate}")
+        return None
+    finally:
+        # Close the connection
+        if connection:
+            connection.close()
